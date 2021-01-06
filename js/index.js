03d03ef4aca50d420f535871b3abdf72a5a24bbd
Naveen Jamdagani <naveen@paralleldots.com>
2021-01-06 17:53:34 +0530

    add experience in portfolio

--- js/index.js
+++ js/index.js
@@ -1,35 +1,44 @@
-$(document).ready(function()
-{
-	var toggleNav = function () {
-		$("#content").toggleClass("active");
-		$("#left-nav-bar").toggleClass("active");
-	}
+let skills =
+  "HTML,CSS,Javascript,jQuery,Git,Front-End Development,SCSS / SASS,AJAX,CSS3,React.js,Redux,Webpack,Tornado web framework,konvaJS,D3.js";
+$(document).ready(function () {
+	let html = ``;
+  skills.split(",").map((d) => {
+    html = html +`<div class="person-info-details">
+		${d}
+	</div>`;
+  });
+  $("#person-skills").html(html)
+  var toggleNav = function () {
+    $("#content").toggleClass("active");
+    $("#left-nav-bar").toggleClass("active");
+  };
 
-	$("#toggle-content").on("click",function()
-	{
-		toggleNav();
-	});
-	$("#toggle-day").on("click",function()
-	{
-		$("#portfolio").toggleClass("active");
-	});
-	$("#toggle-night").on("click",function()
-	{
-		$("#portfolio").toggleClass("active");
-	});
-	$("#slide-down").on("click",function()
-	{
-		$('html,body').animate({
-	        scrollTop: $("#person-objective").offset().top - 100
-	    }, 'slow');
-	});
-	$("#left-nav-bar li").on("click",function()
-	{
-		toggleNav();
-		
-		var id = $(this).data("id");
-		$('html,body').animate({
-	        scrollTop: $("#"+id).offset().top - 100
-	    }, 'slow');
-	});
+  $("#toggle-content").on("click", function () {
+    toggleNav();
+  });
+  $("#toggle-day").on("click", function () {
+    $("#portfolio").toggleClass("active");
+  });
+  $("#toggle-night").on("click", function () {
+    $("#portfolio").toggleClass("active");
+  });
+  $("#slide-down").on("click", function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#person-objective").offset().top - 100,
+      },
+      "slow"
+    );
+  });
+  $("#left-nav-bar li").on("click", function () {
+    toggleNav();
+
+    var id = $(this).data("id");
+    $("html,body").animate(
+      {
+        scrollTop: $("#" + id).offset().top - 100,
+      },
+      "slow"
+    );
+  });
 });
